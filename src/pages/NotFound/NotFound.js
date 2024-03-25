5395463d142ca686eccb2b866e3c177ad4fb1665
merabkandelaki <100033268+merabkandelaki@users.noreply.github.com>
2024-03-25 22:03:06 +0400

    changes deploy

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -3,11 +3,13 @@ import NotFoundPageImg from "../../assets/404_not_found.svg";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
+  window.location.replace("/");
   return (
     <div className={styles.not_found}>
       <div className={styles.image_title}>
         <img src={NotFoundPageImg} alt="" />
         <h1>Page Not Found</h1>
+        
       </div>
     </div>
   );
